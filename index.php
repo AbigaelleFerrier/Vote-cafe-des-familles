82acf38cb1beb5c519df56025adf6f1cec265076
cirill <cirill@asheart.fr>
2021-11-22 19:15:38 +0100

    css

--- index.php
+++ index.php
@@ -126,9 +126,10 @@ $propals = json_decode(file_get_contents('proposition.json'));
         </div>
     </main>
 
-    <a href="">
-       Code source
-    </a>
+    <div class="d-flex text-align justify-content-center mb-5">
+        <a class="mx-2" style="color:white;" href="https://github.com/AbigaelleFerrier/Vote-cafe-des-familles">Code source</a>
+        <a class="mx-2" style="color:white;" href="/vote.csv">Fichier de vote</a>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
